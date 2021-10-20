e884c0800dcb1bef07d52f92cd50c3cbfb8c32e4
Ricardo Perlini <ricardo.perlini@totvs.com.br>
2021-10-20 11:29:10 -0300

    Add controller

---
+++ src/TOTVSIntegrationCRM.Web/Controllers/HealthZController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TOTVSIntegrationCRM.Web.Controllers
+{
+    [Route("/healthz")]
+    public class HealthZController : TnfController
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult Get()
+        {           
+            return CreateResponseOnGet("ok");
+        }       
+    }
+}
---
+++ src/TOTVSIntegrationCRM.Web/Controllers/StatusZController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TOTVSIntegrationCRM.Web.Controllers
+{
+    [Route("/statusz")]
+    public class StatusZController : TnfController
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult Get()
+        {           
+            return CreateResponseOnGet("ok");
+        }       
+    }
+}
