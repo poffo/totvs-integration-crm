b2b68d887335f18d753b8f61b8e7b222241790e9
Ricardo Perlini <ricardo.perlini@totvs.com.br>
2021-10-21 13:41:40 -0300

    Add controller

---
+++ src/TOTVSIntegrationCRM.Web/Controllers/LogsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TOTVSIntegrationCRM.Web.Controllers
+{
+    [Route("/logs")]
+    public class LogsController : TnfController
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult Get()
+        {           
+            return CreateResponseOnGet("ok");
+        }       
+    }
+}
---
+++ src/TOTVSIntegrationCRM.Web/Controllers/RootController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TOTVSIntegrationCRM.Web.Controllers
+{
+    [Route("/")]
+    public class RootController : TnfController
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult Get()
+        {           
+            return CreateResponseOnGet("Running");
+        }       
+    }
+}
